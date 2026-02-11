d42eaf94bcc86f0e93c35131c7431350e6521aad
gaurav <gauravnepal3@gmail.com>
2026-02-11 15:27:55 +0545

    changes

--- run.sh
+++ run.sh
@@ -14,3 +14,5 @@ k6 run rtb-load-test.js \
   -e PRE_ALLOCATED_VUS=50 \
   -e MAX_VUS=100 \
   -e DURATION=120s
+
+  k6 run fake_request.js   -e MODE=constant   -e TARGET_QPS=3800   -e DURATION=120s   -e PRE_ALLOCATED_VUS=12000   -e MAX_VUS=30000   -e TIMEOUT=5s   -e BASE_URL=http://rtb.us.adstork.com   -e SSP_API_KEY=3c8ac91e4b745f0a31f914332437be24
